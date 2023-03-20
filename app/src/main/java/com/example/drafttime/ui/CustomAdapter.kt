c0099fe8b91743d805a9b5d80c44bd7c398fc6ae
Zmoney1999 <91281730+Zmoney1999@users.noreply.github.com>
2023-03-19 17:59:15 -0700

    background updated

--- app/src/main/java/com/example/drafttime/ui/CustomAdapter.kt
+++ app/src/main/java/com/example/drafttime/ui/CustomAdapter.kt
@@ -1,6 +1,3 @@
-import android.annotation.SuppressLint
-import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
@@ -8,7 +5,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.PopupMenu
 import android.widget.TextView
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import com.example.connectedweather.R
 import com.example.drafttime.data.PlayerInfo
@@ -17,7 +13,7 @@ import kotlin.random.Random
 
 class CustomAdapter(val playerdata: List<List<PlayerInfo>>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
-    
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflates the card_view_design view
         // that is used to hold list item
@@ -47,34 +43,32 @@ class CustomAdapter(val playerdata: List<List<PlayerInfo>>) : RecyclerView.Adapt
 
 
 
-
-
-
         val pName: TextView= itemView.findViewById(R.id.name)
         val pAge: TextView = itemView.findViewById(R.id.age)
         var pPosition : TextView = itemView.findViewById(R.id.position)
         val pTeam: TextView = itemView.findViewById(R.id.team_name)
         val optionMenu: Button = itemView.findViewById(R.id.textViewOptions)
-        var playerItem: PlayerInfo? = null
+        private var player : PlayerInfo? = null
 
         ///Handle clicks from the user
 
         init {
             optionMenu.setOnClickListener {
 
+
               val popupMenu = PopupMenu(itemView.context, it)
-                val inflater = popupMenu.menuInflater
                 popupMenu.inflate(R.menu.options_menu)
 
-                //Log.d("ViewHolder" , "Clicked")
                 popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener {item: MenuItem? ->
                     when(item!!.itemId){
                         R.id.menu1->
-                            println("Clicked menu 1")
+                            //TODO: Handle draft player
+                            println(player)
+
                         R.id.menu2->
+                            //TODO: Handle randomize player
                             println("Clicked Menu 2")
-                        R.id.menu3->
-                            println("Clicked menu 3")
+
 
                     }
                     true
@@ -95,7 +89,7 @@ class CustomAdapter(val playerdata: List<List<PlayerInfo>>) : RecyclerView.Adapt
 
         fun bind(playerInfo: List<PlayerInfo>) {
             val value = Random.nextInt(0,10)
-
+            player = playerInfo[value]
             pName.text = playerInfo[value].fullName
             pAge.text = playerInfo[value].age
             pPosition.text = playerInfo[value].postion
